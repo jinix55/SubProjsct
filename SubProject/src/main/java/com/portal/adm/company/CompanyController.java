3b99229633715948cb5a52824ac61495d2e7ac04
swandhw <swandhw@gmail.com>
2022-07-06 01:48:28 +0900

    Update CompanyController.java

--- SubProject/src/main/java/com/portal/adm/company/CompanyController.java
+++ SubProject/src/main/java/com/portal/adm/company/CompanyController.java
@@ -231,10 +231,11 @@ public class CompanyController {
     				MemberModel memberModel = new MemberModel();
     				//저장시 회사코드와 같이 적용
     				memberModel.setUserId("admin@"+companyModel.getCompanyCode());
-    				memberModel.setPassword("admin@"+companyModel.getCompanyCode());
+    				memberModel.setPassword("admin");
 //    				memberModel.setEmail("admin@"+companyModel.getCompanyCode().toLowerCase()+".pplusecho.com");
     				memberModel.setUserNm("사이트 관리자");
     				memberModel.setAuthId("au2000002");
+    				memberModel.setPassInit("Y");
     				memberModel.setDtLimitYn("N");
     				memberModel.setUseYn("Y");
     				memberModel.setCompanyCode(companyModel.getCompanyCode());
@@ -390,8 +391,9 @@ public class CompanyController {
             memberModel.setRgstId(authUser.getMemberModel().getUserId());
             memberModel.setModiId(authUser.getMemberModel().getUserId());
             //저장시 회사코드와 같이 적용
+            memberModel.setPassword(memberModel.getUserId());
             memberModel.setUserId(memberModel.getUserId()+"@"+memberModel.getCompanyCode());	
-            memberModel.setPassword(memberModel.getUserId()+"@"+memberModel.getCompanyCode());
+            memberModel.setPassInit("Y");
             String result = memberService.insert(memberModel);
 
             return new ResponseEntity<>(result, HttpStatus.OK);
